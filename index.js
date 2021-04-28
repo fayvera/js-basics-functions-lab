3d51e47be270c789b4fb72a79b3ff5199beb317f
Fay Vera <fvera.cavalcante@gmail.com>
2021-04-28 17:04:37 -0400

    commit tree 84c57a53c1a1b6689cefa92421765f48b53bf1b3

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+const Hq = 42
+
+function distanceFromHqInBlocks(someValue){
+    if (someValue > 42 ){
+        return (someValue - 42)
+    } else {
+        return (42 - someValue)
+    }
+}
+
+
+function distanceFromHqInFeet(someValue){
+    return (distanceFromHqInBlocks(someValue) * 264)
+}
+
+function distanceTravelledInFeet(start, end){
+    let someValue = start - end
+    if (someValue > 0){
+        return someValue * 264
+    } else {
+        return someValue * -264
+    }
+}
+
+function calculatesFarePrice(start, destination){
+    let someValue = Math.abs(destination - start) * 264
+    if (someValue < 400){
+        return 0
+    } else if (someValue > 2500) {
+        return "cannot travel that far"
+    } else if (someValue > 2000){
+        return 25
+    } else {
+        return ((someValue - 400) * 0.02)
+    }
+}
